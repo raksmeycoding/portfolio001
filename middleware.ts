e50b6c14d10f870c2e58d3149f17074d67c45331
raksmeycoding <raksmeykoung@gmail.com>
2025-01-06 22:53:17 +0700

    raksmey change runtime

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,14 @@
 export { auth as middleware } from "@/auth"
+
+
+
+export const config = {
+    unstable_allowDynamic: [
+        // allows a single file
+        '/models/test.ts',
+        '/models/user.ts',
+        // use a glob to allow anything in the function-bind 3rd party module
+        // '**/node_modules/function-bind/**',
+        // 'mongoose/dist/browser.umd.js',
+    ],
+}
